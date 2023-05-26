5f3e3bd1b6404af5ea40b563937c9f4f0e60df25
William35er <125413709+William35er@users.noreply.github.com>
2023-05-26 18:53:02 +0000

    reactlandingpage

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="footercontainer text-center">
+            <p> Made by William </p>
+
+		</div>
+	);
+};
+
+export default Footer;
