d8326bdef20102660a4613baebc33e05adc5ecd9
William35er <125413709+William35er@users.noreply.github.com>
2023-06-23 14:56:06 +0000

    fixedLandinpage

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const Cards = () => {
 	return (
-		<div class="card" style={{width:"18rem"}}>
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Find out More!</a>
+		<div className="card" style={{width:"18rem"}}>
+  <img src="https://placehold.co/500x325" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Find out More!</a>
   </div>
 </div> 
 		 	
